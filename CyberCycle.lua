187f27165d12e72d07f8940ee04da5ad03ce4a3b
nnh <nick-h@yandex.ru>
2017-02-22 19:12:05 +0300

    Quik 7.6 update

--- CyberCycle.lua
+++ CyberCycle.lua
@@ -1,3 +1,4 @@
+--logfile=io.open("C:\\SBERBANK\\QUIK_SMS\\LuaIndicators\\qlua_log.txt", "w")
 
 Settings = 
 {
@@ -7,21 +8,57 @@ Settings =
 	line=
 	{
 		{
-			Name = "Cycle",
-			Color = RGB(0, 128, 0),
-			Type = TYPE_LINE,
-			Width = 2
-		}
-	,
-		{
 			Name = "Trigger Line",
 			Color = RGB(255, 0, 0),
+			Type = TYPE_DASHDOT,
+			Width = 1
+		},
+		{
+			Name = "Cycle",
+			Color = RGB(0, 128, 0),
 			Type = TYPE_LINE,
 			Width = 2
 		}
 	}
 }
 
+-- Пользовательcкие функции
+function WriteLog(text)
+
+   logfile:write(tostring(os.date("%c",os.time())).." "..text.."\n");
+   logfile:flush();
+   LASTLOGSTRING = text;
+
+end;
+
+function toYYYYMMDDHHMMSS(datetime)
+   if type(datetime) ~= "table" then
+      --message("в функции toYYYYMMDDHHMMSS неверно задан параметр: datetime="..tostring(datetime))
+      return ""
+   else
+      local Res = tostring(datetime.year)
+      if #Res == 1 then Res = "000"..Res end
+      local month = tostring(datetime.month)
+      if #month == 1 then Res = Res.."0"..month; else Res = Res..month; end
+      local day = tostring(datetime.day)
+      if #day == 1 then Res = Res.."0"..day; else Res = Res..day; end
+      local hour = tostring(datetime.hour)
+      if #hour == 1 then Res = Res.."0"..hour; else Res = Res..hour; end
+      local minute = tostring(datetime.min)
+      if #minute == 1 then Res = Res.."0"..minute; else Res = Res..minute; end
+      local sec = tostring(datetime.sec);
+      if #sec == 1 then Res = Res.."0"..sec; else Res = Res..sec; end;
+      return Res
+   end
+end --toYYYYMMDDHHMMSS
+
+function isnil(a,b)
+   if a == nil then
+      return b
+   else
+      return a
+   end;
+end;
 ----------------------------------------------------------
 
 function CyberCycle()
@@ -59,7 +96,6 @@ function CyberCycle()
 			I1={}
 			DeltaPhase={}
 			
-			Price[index] = (H(index) + L(index))/2
 			Smooth[index]=0
 			Cycle[index]=0
 			aCycle[index]=0
@@ -69,19 +105,41 @@ function CyberCycle()
 			Q1[index]=0
 			I1[index]=0
 			DeltaPhase[index]=0
+			
+			if CandleExist(index) then
+				Price[index] = (H(index) + L(index))/2
+			else 
+				Price[index] = 0
+			end
+			
+			return nil, nil
+		end
+		
+		if not CandleExist(index) then
+			Price[index]=Price[index-1]
+			Cycle[index]=Cycle[index-1]
+			aCycle[index]=aCycle[index-1]
+			CyclePeriod[index]=CyclePeriod[index - 1]
+			InstPeriod[index]=InstPeriod[index-1]
+			Q1[index]=Q1[index-1]
+			I1[index]=I1[index-1]
+			DeltaPhase[index]=DeltaPhase[index-1]
+			Smooth[index]=Smooth[index-1]
+			Trigger[index]=Trigger[index-1]
 			return nil, nil
 		end
 		
 		Price[index] = (H(index) + L(index))/2
 		
 		if index < 4 then
-			Cycle[index]=0
-			aCycle[index]=0
-			CyclePeriod[index]=0
-			InstPeriod[index]=0
-			Q1[index]=0
-			I1[index]=0
-			DeltaPhase[index]=0
+			Cycle[index]=Cycle[index-1]
+			aCycle[index]=aCycle[index-1]
+			CyclePeriod[index]=CyclePeriod[index -1]
+			InstPeriod[index]=InstPeriod[index-1]
+			Q1[index]=Q1[index-1]
+			I1[index]=I1[index-1]
+			DeltaPhase[index]=DeltaPhase[index-1]
+			Smooth[index]=Smooth[index-1]
 			return nil, nil
 		end
 		
@@ -89,23 +147,25 @@ function CyberCycle()
 		Cycle[index]=(Price[index]-2.0*Price[index - 1]+Price[index - 2])/4.0
 		aCycle[index]=(Price[index]-2.0*Price[index - 1]+Price[index - 2])/4.0
 		
+		
 		if index < 7 then
-			aCycle[index]=0
-			CyclePeriod[index]=0
-			InstPeriod[index]=0
-			Q1[index]=0
-			I1[index]=0
-			DeltaPhase[index]=0
+			Cycle[index]=Cycle[index-1]
+			aCycle[index]=aCycle[index-1]
+			CyclePeriod[index]=CyclePeriod[index-1]
+			InstPeriod[index]=InstPeriod[index-1]
+			Q1[index]=Q1[index-1]
+			I1[index]=I1[index-1]
+			DeltaPhase[index]=DeltaPhase[index-1]
 			return nil, nil
 		end
 					
 		Cycle[index]=(1.0-0.5*alpha) *(1.0-0.5*alpha) *(Smooth[index]-2.0*Smooth[index - 1]+Smooth[index - 2])
 						+2.0*(1.0-alpha)*Cycle[index - 1]-(1.0-alpha)*(1.0-alpha)*Cycle[index - 2]			   
-
+		
 		Trigger[index] = Cycle[index-1]	
 		
 		if cycletype == 0 then
-			return Cycle[index], Trigger[index]
+			return Trigger[index], Cycle[index] 
 		end
 		        
 		Q1[index] = (0.0962*Cycle[index]+0.5769*Cycle[index-2]-0.5769*Cycle[index-4]-0.0962*Cycle[index-6])*(0.5+0.08*(InstPeriod[index-1] or 0))
@@ -135,7 +195,7 @@ function CyberCycle()
         Trigger[index] = CyclePeriod[index-1]
 				
 		if cycletype == 1 then
-			return CyclePeriod[index], Trigger[index]
+			return Trigger[index], CyclePeriod[index] 
 		end
 		
 		alpha1 = 2.0/(CyclePeriod[index]+1.0)
@@ -143,7 +203,7 @@ function CyberCycle()
 		aCycle[index]=(1.0-0.5*alpha1) *(1.0-0.5*alpha1) *(Smooth[index]-2.0*Smooth[index - 1]+Smooth[index - 2])
 						+2.0*(1.0-alpha1)*aCycle[index - 1]-(1.0-alpha1)*(1.0-alpha1)*aCycle[index - 2]			   
 		
-		return aCycle[index], aCycle[index-1]
+		return aCycle[index-1], aCycle[index] 
 	end
 end
 	----------------------------
@@ -155,6 +215,7 @@ end
 
 function OnCalculate(index)
 
+	--WriteLog ("OnCalc() ".."CandleExist("..index.."): "..tostring(CandleExist(index)).."; T("..index.."); "..isnil(toYYYYMMDDHHMMSS(T(index))," - ").."; C("..index.."): "..isnil(C(index),"-"));
 	return myCyberCycle(index, Settings.alpha, Settings.cycletype)
 end
 
