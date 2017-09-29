84f6a1f876f67701753a377944e8241e494fc0b4
nnh <nick-h@yandex.ru>
2017-09-29 20:35:32 +0300

    Update tema.lua

--- tema.lua
+++ tema.lua
@@ -1,21 +1,36 @@
 Settings =
  {
-    Name = "*TEMA",
- 	period = 21,
-  	vType = "T",
-   line =
+	Name = "*TEMA",
+	periodTEMA = 112,
+	vTypeTEMA = "C",
+	periodEMA = 64,
+	vTypeEMA = "C",
+	showTEMA = 1,
+	showEMA = 1,
+	line =
      {
          {
-          Name = "Line",
+          Name = "TEMA",
           Color = RGB(255,0,0),
           Type = TYPE_LINE,
-          Width =1
+          Width =2
+         },
+         {
+          Name = "EMA",
+          Color = RGB(128,0,0),
+          Type = TYPE_LINE,
+          Width =2
          }
      }
  }
 
 function dValue(i,param)
 local v = param or "C"
+	
+	if not CandleExist(i) then
+		return nil
+	end
+	
 	if  v == "O" then
 		return O(i)
 	elseif   v == "H" then
@@ -33,12 +48,32 @@ local v = param or "C"
 	elseif   v == "W" then
 		return (H(i) + L(i)+2*C(i))/4
 	elseif   v == "ATR" then
-		return math.max(math.abs(H(i) - L(i)), math.abs(H(i) - C(i-1)), math.abs(C(i-1) - L(i)))
+		local previous = math.max(i-1, 1)
+			
+		if not CandleExist(previous) then
+			previous = FindExistCandle(previous)
+		end
+		if previous == 0 then
+			return 0
+		end
+	
+		return math.max(math.abs(H(i) - L(i)), math.abs(H(i) - C(previous)), math.abs(C(previous) - L(i)))
 	else
 		return C(i)
 	end 
 end
 
+function FindExistCandle(I)
+
+	local out = I
+	
+	while not CandleExist(out) and out > 0 do
+		out = out -1
+	end	
+	
+	return out
+ 
+end
 
  function Init()
 	myTEMA = TEMA()
@@ -46,54 +81,80 @@ end
  end
 
  function OnCalculate(index)
-	return myTEMA(index, Settings.period, Settings.vType)
+	return myTEMA(index, Settings)
  end
  
  function TEMA()
 	
-	local cache_EMA1={}
-	local cache_EMA2={}
-	local cache_EMA3={}
+	local cache_EMA={}
+	local cache_TEMA1={}
+	local cache_TEMA2={}
+	local cache_TEMA3={}
 	
-	return function(ind, _p, _v)
+	return function(ind, Fsettings)
+		
+		local Fsettings=(Fsettings or {})
 		local index = ind
-		local period = _p
-		local vType = _v
-		local k = 2/(_p+1)
-		local value = 0
-		local out = nil
+		local periodTEMA = Fsettings.periodTEMA or 112
+		local showTEMA = Fsettings.showTEMA or 1
+		local vTypeTEMA = Fsettings.vTypeTEMA or "C"
+		local periodEMA = Fsettings.periodEMA or 63
+		local showEMA = Fsettings.showEMA or 1
+		local vTypeEMA = Fsettings.vTypeEMA or "C"
+
+		local kTEMA = 2/(periodTEMA+1)
+		local kEMA = 2/(periodEMA+1)
+		
+		local valueTEMA = 0
+		local valueEMA = 0
+		
+		local outTEMA = nil
+		local outEMA = nil
 
 		if index == 1 then
-			cache_EMA1 = {}
-			cache_EMA2 = {}
-			cache_EMA3 = {}
+			cache_EMA = {}
+			cache_TEMA1 = {}
+			cache_TEMA2 = {}
+			cache_TEMA3 = {}
 			if CandleExist(index) then
-				cache_EMA1[index]= dValue(index, vType)
-				cache_EMA2[index]= dValue(index, vType)
-				cache_EMA3[index]= dValue(index, vType)
+				cache_EMA[index]= dValue(index, vTypeEMA)
+				cache_TEMA1[index]= dValue(index, vTypeTEMA)
+				cache_TEMA2[index]= dValue(index, vTypeTEMA)
+				cache_TEMA3[index]= dValue(index, vTypeTEMA)
 			else 
-				cache_EMA1[index]= 0
-				cache_EMA2[index]= 0
-				cache_EMA3[index]= 0
+				cache_EMA[index]= 0
+				cache_TEMA1[index]= 0
+				cache_TEMA2[index]= 0
+				cache_TEMA3[index]= 0
 			end
-			return out
+			return nil
 		end
 		
+		cache_EMA[index] = cache_EMA[index-1] 
+		cache_TEMA1[index] = cache_TEMA1[index-1] 
+		cache_TEMA2[index] = cache_TEMA2[index-1]
+		cache_TEMA3[index] = cache_TEMA3[index-1]
+		
 		if not CandleExist(index) then
-			cache_EMA1[index] = cache_EMA1[index-1] 
-			cache_EMA2[index] = cache_EMA2[index-1]
-			cache_EMA3[index] = cache_EMA3[index-1]
 			return nil
 		end
 
-		value = dValue(index, vType)	
-		cache_EMA1[index]=k*value+(1-k)*cache_EMA1[index-1]
-		cache_EMA2[index]=k*cache_EMA1[index]+(1-k)*cache_EMA2[index-1]
-		cache_EMA3[index]=k*cache_EMA2[index]+(1-k)*cache_EMA3[index-1]
+		valueTEMA = dValue(index, vTypeTEMA)	
+		cache_TEMA1[index]=kTEMA*valueTEMA+(1-kTEMA)*cache_TEMA1[index-1]
+		cache_TEMA2[index]=kTEMA*cache_TEMA1[index]+(1-kTEMA)*cache_TEMA2[index-1]
+		cache_TEMA3[index]=kTEMA*cache_TEMA2[index]+(1-kTEMA)*cache_TEMA3[index-1]
 		
-		out = 3*cache_EMA1[index] - 3*cache_EMA2[index] + cache_EMA3[index]
+		valueEMA = dValue(index, vTypeEMA)	
+		cache_EMA[index]=kEMA*valueEMA+(1-kEMA)*cache_EMA[index-1]
 		
-		return out
+		if showTEMA == 1 then
+			outTEMA = 3*cache_TEMA1[index] - 3*cache_TEMA2[index] + cache_TEMA3[index]
+		end
+		if showEMA == 1 then
+			outEMA = cache_EMA[index]
+		end
+		
+		return outTEMA, outEMA
 			
 	end
 end
