442d02c6ac1818af4bbdbbf190e5de2f43405daf
nnh <nick-h@yandex.ru>
2018-03-09 18:50:34 +0300

    Update monitorReg.lua

--- scriptMonitor/monitorReg.lua
+++ scriptMonitor/monitorReg.lua
@@ -1,168 +1,169 @@
 function initReg()
-    calcAlgoValue = nil     --      Возвращаемая таблица
-    fx_buffer = nil         --      Линия регрессии
-    sql_buffer = nil    --      +Сигма
-    sqh_buffer = nil    --      -Сигма
+    calcAlgoValue = nil     --      Р’РѕР·РІСЂР°С‰Р°РµРјР°СЏ С‚Р°Р±Р»РёС†Р°
+    fx_buffer = nil         --      Р›РёРЅРёСЏ СЂРµРіСЂРµСЃСЃРёРё
+    sql_buffer = nil    --      +РЎРёРіРјР°
+    sqh_buffer = nil    --      -РЎРёРіРјР°
     sx = nil
 end
 
-function Reg(index, settings, DS)
+function Reg(iSec, index, settings, DS)
  	        		
-		local bars = settings.bars or 182
-		local degree = settings.degree or 1
-		local kstd = settings.kstd or 3
-        
-        if index == nil then index = DS:Size() end
-		bars = math.min(bars, DS:Size()) - 2
-		
-		local p = 0
-		local n = 0
-		local f = 0
-		local qq = 0
-		local mm = 0
-		local tt = 0
-		local ii = 0
-		local jj = 0
-		local kk = 0
-		local ll = 0
-		local nn = 0
-		local sq = 0
-		local i0 = 0
-		
-		local mi = 0
- 		local ai={{1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}}		
-		local b={}
-		local x={}
-		
-		p = bars 
-		nn = degree+1
+	local bars = settings.bars or 182
+	local degree = settings.degree or 1
+	local kstd = settings.kstd or 3
+    
+    if index == nil then index = DS:Size() end
+	bars = math.min(bars, DS:Size())
+	
+	local p = 0
+	local n = 0
+	local f = 0
+	local qq = 0
+	local mm = 0
+	local tt = 0
+	local ii = 0
+	local jj = 0
+	local kk = 0
+	local ll = 0
+	local nn = 0
+	local sq = 0
+	local i0 = 0
+	
+	local mi = 0
+ 	local ai={{1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}}		
+	local b={}
+	local x={}
+	
+	p = bars 
+	nn = degree+1
  
-        sql_buffer = {}
-        sqh_buffer = {}
-		sql_buffer[index]= 0
-		sqh_buffer[index]= 0
-            
-		calcAlgoValue = {}
-        calcAlgoValue[index]= 0
-            
-		fx_buffer = {}
-        fx_buffer[index]= 0
+    sql_buffer = {}
+    sqh_buffer = {}
+	sql_buffer[index]= 0
+	sqh_buffer[index]= 0
+        
+	calcAlgoValue = {}
+    calcAlgoValue[index]= 0
+        
+	fx_buffer = {}
+    fx_buffer[index]= 0
 
-		--- sx 
-		sx={}
-		sx[1] = p+1
-			
-		for mi=1, nn*2-2 do
-			sum=0
-			for n=i0, i0+p do
-				sum = sum + math.pow(n,mi)
-			end
-			sx[mi+1]=sum
-		end
-               		 
-		--- syx 
-		for mi=1, nn do
-			sum = 0
-			for n=i0, i0+p do
-				if DS:C(index+n-bars) ~= nil then
-					if mi==1 then
-					   sum = sum + DS:C(index+n-bars)
-					else
-					   sum = sum + DS:C(index+n-bars)*math.pow(n,mi-1)
-					end
-				end
-			end
-		    b[mi]=sum
-		end
-			 
-		--- Matrix 
-		for jj=1, nn do
-			for ii=1, nn do
-				kk=ii+jj-1
-				ai[ii][jj]=sx[kk]
-			end
-		end
-			 
-		--- Gauss 
-		for kk=1, nn-1 do
-			ll=0
-			mm=0
-			for ii=kk, nn do
-				if math.abs(ai[ii][kk])>mm then
-					mm=math.abs(ai[ii][kk])
-					ll=ii
-				end
-			end
-				
-			if ll==0 then
-				return calcAlgoValue
-			end
-			if ll~=kk then
-
-				for jj=1, nn do
-					tt=ai[kk][jj]
-					ai[kk][jj]=ai[ll][jj]
-					ai[ll][jj]=tt
-				end
-				tt=b[kk]
-				b[kk]=b[ll]
-				b[ll]=tt
-			end
-			for ii=kk+1, nn do
-				qq=ai[ii][kk]/ai[kk][kk]
-				for jj=1, nn do
-					if jj==kk then
-						ai[ii][jj]=0
-					else
-						ai[ii][jj]=ai[ii][jj]-qq*ai[kk][jj]
-					end
-				end
-				b[ii]=b[ii]-qq*b[kk]
-			end
-		end
-		   
-		 x[nn]=b[nn]/ai[nn][nn]
-		   
-		for ii=nn-1, 1, -1 do
-			tt=0
-			for jj=1, nn-ii do
-				tt=tt+ai[ii][ii+jj]*x[ii+jj]
-				x[ii]=(1/ai[ii][ii])*(b[ii]-tt)
-			end
-		end
-		   
-		---
+	--- sx 
+	sx={}
+	sx[1] = p+1
+		
+	for mi=1, nn*2-2 do
+		sum=0
 		for n=i0, i0+p do
-			sum=0
-			for kk=1, degree do
-				sum = sum + x[kk+1]*math.pow(n,kk)
-			end
-			fx_buffer[n]=x[1]+sum
-            calcAlgoValue[index+n-bars] = round(fx_buffer[n], 5)
- 		end
-			 
-		--- Std 
-        sq=0.0
+			sum = sum + math.pow(n,mi)
+		end
+		sx[mi+1]=sum
+	end
+           		 
+	--- syx 
+	for mi=1, nn do
+		sum = 0
 		for n=i0, i0+p do
 			if DS:C(index+n-bars) ~= nil then
-				sq = sq + math.pow(DS:C(index+n-bars)-fx_buffer[n],2)
+				if mi==1 then
+				   sum = sum + DS:C(index+n-bars)
+				else
+				   sum = sum + DS:C(index+n-bars)*math.pow(n,mi-1)
+				end
 			end
 		end
-		   
-		sq = math.sqrt(sq/(p+1))*kstd
+	    b[mi]=sum
+	end
+		 
+	--- Matrix 
+	for jj=1, nn do
+		for ii=1, nn do
+			kk=ii+jj-1
+			ai[ii][jj]=sx[kk]
+		end
+	end
+		 
+	--- Gauss 
+	for kk=1, nn-1 do
+		ll=0
+		mm=0
+		for ii=kk, nn do
+			if math.abs(ai[ii][kk])>mm then
+				mm=math.abs(ai[ii][kk])
+				ll=ii
+			end
+		end
+			
+		if ll==0 then
+			return calcAlgoValue
+		end
+		if ll~=kk then
 
-		for n=i0, i0+p do
-			sqh_buffer[index+n-bars]=round(fx_buffer[n]+sq, 5)
-			sql_buffer[index+n-bars]=round(fx_buffer[n]-sq, 5)
- 		end        
-        		
-		return calcAlgoValue
+			for jj=1, nn do
+				tt=ai[kk][jj]
+				ai[kk][jj]=ai[ll][jj]
+				ai[ll][jj]=tt
+			end
+			tt=b[kk]
+			b[kk]=b[ll]
+			b[ll]=tt
+		end
+		for ii=kk+1, nn do
+			qq=ai[ii][kk]/ai[kk][kk]
+			for jj=1, nn do
+				if jj==kk then
+					ai[ii][jj]=0
+				else
+					ai[ii][jj]=ai[ii][jj]-qq*ai[kk][jj]
+				end
+			end
+			b[ii]=b[ii]-qq*b[kk]
+		end
+	end
+	   
+	 x[nn]=b[nn]/ai[nn][nn]
+	   
+	for ii=nn-1, 1, -1 do
+		tt=0
+		for jj=1, nn-ii do
+			tt=tt+ai[ii][ii+jj]*x[ii+jj]
+			x[ii]=(1/ai[ii][ii])*(b[ii]-tt)
+		end
+	end
+	   
+	---
+	for n=i0, i0+p do
+		sum=0
+		for kk=1, degree do
+			sum = sum + x[kk+1]*math.pow(n,kk)
+		end
+		fx_buffer[n]=x[1]+sum
+        calcAlgoValue[index+n-bars] = round(fx_buffer[n], 5)
+ 	end
+		 
+	--- Std 
+    sq=0.0
+	for n=i0, i0+p do
+		if DS:C(index+n-bars) ~= nil then
+			sq = sq + math.pow(DS:C(index+n-bars)-fx_buffer[n],2)
+		end
+	end
+	   
+	sq = math.sqrt(sq/(p+1))*kstd
+
+	for n=i0, i0+p do
+		sqh_buffer[index+n-bars]=round(fx_buffer[n]+sq, 5)
+		sql_buffer[index+n-bars]=round(fx_buffer[n]-sq, 5)
+ 	end        
+    		
+	return calcAlgoValue
 	
 end
 
 function signalReg(i, cell, settings, DS, signal)
     
-    local testvalue = tonumber(getParamEx(CLASS_CODE,SEC_CODE,"last").param_value) or 0
+    --tonumber(getParamEx(CLASS_CODE,SEC_CODE,"last").param_value) or 0
+	local testvalue = GetCell(t_id, i, tableIndex["РўРµРєСѓС‰Р°СЏ С†РµРЅР°"]).value
 	local scale = getSecurityInfo(CLASS_CODE, SEC_CODE).scale
 	--local price_step = tonumber(getParamEx(CLASS_CODE, SEC_CODE, "SEC_PRICE_STEP").param_value) or 0
     local signaltestvalue1 = calcAlgoValue[DS:Size()-1] or 0
@@ -183,11 +184,11 @@ function signalReg(i, cell, settings, DS, signal)
         local colorGradation = math.floor((math.abs(testvalue - calcVal)/deltaSigma)*(255-200))
         local Color = RGB(255, 255, 255)
         if calcVal>=testvalue then
-            Color = RGB(math.max(255 - 0.5*colorGradation, 255), 255 - 3*colorGradation, 255 - 3*colorGradation) -- оттенки красного
+            Color = RGB(math.max(255 - 0.5*colorGradation, 255), 255 - 3*colorGradation, 255 - 3*colorGradation) -- РѕС‚С‚РµРЅРєРё РєСЂР°СЃРЅРѕРіРѕ
 		elseif calcVal<testvalue then
-            Color = RGB(255 - 3*colorGradation, math.max(255 - 0.7*colorGradation, 255), 255 - 3.4*colorGradation) --оттенки зеленого
+            Color = RGB(255 - 3*colorGradation, math.max(255 - 0.7*colorGradation, 255), 255 - 3.4*colorGradation) --РѕС‚С‚РµРЅРєРё Р·РµР»РµРЅРѕРіРѕ
 		elseif calcVal>maxSigmaZone or calcVal<minSigmaZone  then
-            Color = RGB(200, 200, 255) --голубой
+            Color = RGB(200, 200, 255) --РіРѕР»СѓР±РѕР№
         end
 
         SetCell(t_id, i, tableIndex[cell], tostring(calcVal), calcVal)
@@ -201,42 +202,42 @@ function signalReg(i, cell, settings, DS, signal)
 		local mes = ""
 		
 		if signaltestvalue1 < DS:C(DS:Size()-1) and signaltestvalue2 > DS:C(DS:Size()-2) then
-			mes = mes0..": Сигнал Buy"
+			mes = mes0..": РЎРёРіРЅР°Р» Buy"
 			myLog(mes)
-			--myLog("Значение алгоритма -1 "..tostring(signaltestvalue1).." Закрытие свечи-1 "..DS:C(DS:Size()-1))
-			--myLog("Значение алгоритма -2 "..tostring(signaltestvalue2).." Закрытие свечи-2 "..DS:C(DS:Size()-2))
+			--myLog("Р—РЅР°С‡РµРЅРёРµ Р°Р»РіРѕСЂРёС‚РјР° -1 "..tostring(signaltestvalue1).." Р—Р°РєСЂС‹С‚РёРµ СЃРІРµС‡Рё-1 "..DS:C(DS:Size()-1))
+			--myLog("Р—РЅР°С‡РµРЅРёРµ Р°Р»РіРѕСЂРёС‚РјР° -2 "..tostring(signaltestvalue2).." Р—Р°РєСЂС‹С‚РёРµ СЃРІРµС‡Рё-2 "..DS:C(DS:Size()-2))
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
 		end
 		if signaltestvalue1 > DS:C(DS:Size()-1) and signaltestvalue2 < DS:C(DS:Size()-2) then
-			mes = mes0..": Сигнал Sell"
+			mes = mes0..": РЎРёРіРЅР°Р» Sell"
 			myLog(mes)
-			--myLog("Значение алгоритма -1 "..tostring(signaltestvalue1).." Закрытие свечи-1 "..DS:C(DS:Size()-1))
-			--myLog("Значение алгоритма -2 "..tostring(signaltestvalue2).." Закрытие свечи-2 "..DS:C(DS:Size()-2))
+			--myLog("Р—РЅР°С‡РµРЅРёРµ Р°Р»РіРѕСЂРёС‚РјР° -1 "..tostring(signaltestvalue1).." Р—Р°РєСЂС‹С‚РёРµ СЃРІРµС‡Рё-1 "..DS:C(DS:Size()-1))
+			--myLog("Р—РЅР°С‡РµРЅРёРµ Р°Р»РіРѕСЂРёС‚РјР° -2 "..tostring(signaltestvalue2).." Р—Р°РєСЂС‹С‚РёРµ СЃРІРµС‡Рё-2 "..DS:C(DS:Size()-2))
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
 		end
 		
 		if testvalue > maxSigmaZone and DS:C(DS:Size()-1) < maxSigmaZone then
-			mes = mes0..": Цена приблизилась к верхней зоне +"..tostring(settings.kstd).." сигма "..tostring(maxSigmaZone)
+			mes = mes0..": Р¦РµРЅР° РїСЂРёР±Р»РёР·РёР»Р°СЃСЊ Рє РІРµСЂС…РЅРµР№ Р·РѕРЅРµ +"..tostring(settings.kstd).." СЃРёРіРјР° "..tostring(maxSigmaZone)
 			myLog(mes)
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
 		end
 	if testvalue < minSigmaZone and DS:C(DS:Size()-1) > minSigmaZone then
-			mes = mes0..": Цена приблизилась к нижней зоне -"..tostring(settings.kstd).." сигма "..tostring(minSigmaZone)
+			mes = mes0..": Р¦РµРЅР° РїСЂРёР±Р»РёР·РёР»Р°СЃСЊ Рє РЅРёР¶РЅРµР№ Р·РѕРЅРµ -"..tostring(settings.kstd).." СЃРёРіРјР° "..tostring(minSigmaZone)
 			myLog(mes)
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
 		end
 		if testvalue < maxSigmaZone and DS:C(DS:Size()-1) > maxSigmaZone then
-			mes = mes0..": Цена оттолкнулась от верхней зоны +"..tostring(settings.kstd).." сигма "..tostring(maxSigmaZone)
+			mes = mes0..": Р¦РµРЅР° РѕС‚С‚РѕР»РєРЅСѓР»Р°СЃСЊ РѕС‚ РІРµСЂС…РЅРµР№ Р·РѕРЅС‹ +"..tostring(settings.kstd).." СЃРёРіРјР° "..tostring(maxSigmaZone)
 			myLog(mes)
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
 		end
 	if testvalue > minSigmaZone and DS:C(DS:Size()-1) < minSigmaZone then
-			mes = mes0..": Цена оттолкнулась от нижней зоны -"..tostring(settings.kstd).." сигма "..tostring(minSigmaZone)
+			mes = mes0..": Р¦РµРЅР° РѕС‚С‚РѕР»РєРЅСѓР»Р°СЃСЊ РѕС‚ РЅРёР¶РЅРµР№ Р·РѕРЅС‹ -"..tostring(settings.kstd).." СЃРёРіРјР° "..tostring(minSigmaZone)
 			myLog(mes)
 			if isMessage == 1 then message(mes) end
 			if isPlaySound == 1 then PaySoundFile(soundFileName) end
