c457923bc802425498c594fac226272129ef4c6a
nnh <nick-h@yandex.ru>
2018-10-13 18:38:11 +0300

    Update monitorVolume.lua

--- scriptMonitor/monitorVolume.lua
+++ scriptMonitor/monitorVolume.lua
@@ -1,5 +1,6 @@
 function initVolume()
     lastVolume = {}
+    lastPrice = {}
     volumeEMA = {}
 end
 
@@ -7,24 +8,37 @@ function Volume(iSec)
 
     local seccode = SEC_CODES['sec_codes'][iSec]
     local classcode = SEC_CODES['class_codes'][iSec]
-    local period = 10                             -- период усреднения объема        
-    local volumeFactor = 4                        -- превышение раз, чтоюы считать объем повышенным        
+    local period = 5                             -- период усреднения объема        
+    local volumeFactor = 3.5                        -- превышение раз, чтоюы считать объем повышенным        
     local k = 2/(period+1)
+    local isMessage = SEC_CODES['isMessage'][iSec]
+    local isPlaySound = SEC_CODES['isPlaySound'][iSec]       
     
     if volumeEMA[seccode] == nil then
         volumeEMA[seccode] = {}
     end
     
     local lastSECVolume = tonumber(getParamEx(classcode,seccode,"valtoday").param_value)
+    local lastSECPrice = tonumber(getParamEx(classcode,seccode,"last").param_value)
     --myLog(SEC_CODES['names'][iSec].." lastSECVolume: "..tostring(lastSECVolume))
     
     if lastVolume[seccode] == nil then
         lastVolume[seccode] = lastSECVolume
+        lastPrice[seccode] = lastSECPrice
     end
+    
     local intervalVolume = lastSECVolume - lastVolume[seccode]
     volumeEMA[seccode][#volumeEMA[seccode] + 1] = intervalVolume
     lastVolume[seccode] = lastSECVolume
-
+    
+    local mes0 = tostring(SEC_CODES['names'][iSec])   
+    if math.abs(lastPrice[seccode] - lastSECPrice)*100/lastPrice[seccode] >= 1 then
+        local mes = mes0..": значительное изменение цены: ".."новая цена: "..tostring(lastSECPrice)..", прошлая цена: "..tostring(lastPrice[seccode])
+        myLog(mes)
+        if isMessage == 1 then message(mes) end
+        if isPlaySound == 1 then PaySoundFile(soundFileName) end
+    end
+    lastPrice[seccode] = lastSECPrice
  
     local ind = #volumeEMA[seccode]
     if ind==period then
@@ -35,10 +49,9 @@ function Volume(iSec)
         volumeEMA[seccode][ind] = sum/(ind-1)
     elseif ind>period then
         volumeEMA[seccode][ind]=round(k*volumeEMA[seccode][ind]+(1-k)*volumeEMA[seccode][ind-1], 5)
+        --myLog(SEC_CODES['names'][iSec].." intervalVolume: "..tostring(intervalVolume))
+        --myLog(SEC_CODES['names'][iSec].." volumeEMA: "..tostring(volumeEMA[seccode][ind]))
         
-        local isMessage = SEC_CODES['isMessage'][iSec]
-        local isPlaySound = SEC_CODES['isPlaySound'][iSec]
-        local mes0 = tostring(SEC_CODES['names'][iSec])
         if intervalVolume > volumeEMA[seccode][ind]*volumeFactor then
             local mes = mes0..": прошел повышенный тиковый объем"
             myLog(mes)
