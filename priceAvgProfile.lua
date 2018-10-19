1248961eb960e8c48c5561ec1e1bfcd6b618c16f
nnh <nick-h@yandex.ru>
2018-10-19 14:17:11 +0300

    Update priceAvgProfile.lua

--- priceAvgProfile.lua
+++ priceAvgProfile.lua
@@ -1,3 +1,5 @@
+-- nnh Glukk Inc. nick-h@yandex.ru
+
 --logfile=io.open(getWorkingFolder().."\\LuaIndicators\\priceAvgProfile.txt", "w")
 
 Settings={}
@@ -5,7 +7,8 @@ Settings.period = 150
 Settings.shift = 100
 Settings.Name = "*priceAvgProfile"
 Settings.weeks = 0 -- 1 - текущая, отрицательное число - сколько прошлых недель, включая текущую
-
+Settings.fixShift = 1 -- 1 - всегда смещено на указанное количество shift, если 0, то будет смещено на дату начала неделеи расчета
+Settings.showMaxLine = 1
 ---------------------------------------------------------------------------------------
 
 lines = 100
@@ -14,9 +17,11 @@ min_price_step = 1
 
 function Init()
 	Settings.line = {}
+	Settings.line[1] = {}
+	Settings.line[1] = {Name = 'maxVol', Color = RGB(255, 128, 64), Type = TYPE_LINE, Width = 2}
 	for i = 1, lines do
-		Settings.line[i] = {}
-		Settings.line[i] = {Color = RGB(185, 185, 185), Type = TYPE_LINE, Width = 2}
+		Settings.line[i+1] = {}
+		Settings.line[i+1] = {Color = RGB(185, 185, 185), Type = TYPE_LINE, Width = 2}
 	end
 	
 	myFFF = FFF()
@@ -39,6 +44,7 @@ function FFF()
 	local cacheH={}
     local cacheC={}
     local weeksBegin={}
+    local maxPriceLine={}
 
 	local outlines = {}
 	local calculated_buffer={}
@@ -48,11 +54,16 @@ function FFF()
         local period = Fsettings.period or 150
         local shift = Fsettings.shift or 150
         local weeks = Fsettings.weeks or 0
+        local fixShift = Fsettings.fixShift or 0
+        local showMaxLine = Fsettings.showMaxLine or 0
         local bars = 50
+
+		shift = math.max(bars+1, shift)
 
 		local index = ind
 
 		if index == 1 then			
+            maxPriceLine = {}
             weeksBegin = {}
             cacheL = {}
             cacheL[index] = 0			
@@ -68,12 +79,13 @@ function FFF()
 		end
 	------------------------------		
 
+		--maxPriceLine[index] = maxPriceLine[index-1] 
 		cacheL[index] = cacheL[index-1] 
 		cacheH[index] = cacheH[index-1] 
 		cacheC[index] = cacheC[index-1] 
 
 		if not CandleExist(index) then
-			return nil
+			return maxPriceLine[index]
 		end
 
 		cacheH[index] = H(index)
@@ -87,13 +99,18 @@ function FFF()
 		if index < Size() then return nil end	
 
 		if calculated_buffer[index] ~= nil then
-			return nil
+			return maxPriceLine[index]
+		end
+
+		if showMaxLine==1 then
+			SetValue(index-shift-1, 1, nil)
+			SetValue(index-shift,   1, nil)
 		end
 
 		for i=1,#outlines do                   		
-			SetValue(index-shift-1,          i, nil)
-			SetValue(index-shift,            i, nil)
-			SetValue(outlines[i].index,   	 i, nil)
+			SetValue(index-shift-1,          i+1, nil)
+			SetValue(index-shift,            i+1, nil)
+			SetValue(outlines[i].index,   	 i+1, nil)
 			
 			outlines[i].index = index-shift
 			outlines[i].val = nil
@@ -107,7 +124,9 @@ function FFF()
 			beginIndex = weeksBegin[#weeksBegin+weeks] or beginIndex
 		end
 		
-		shift = math.max(bars, index-beginIndex)
+		if fixShift==0 then
+			shift = math.max(bars+1, index-beginIndex)
+		end
 
 		--WriteLog('weeks '..tostring(weeks)..' last '..tostring(weeksBegin[#weeksBegin])..' beginIndex '..tostring(beginIndex))
 		
@@ -116,7 +135,7 @@ function FFF()
 		
 		----------------------------------------
 		local priceProfile = {}
-		local clasterStep = math.max((maxPrice - minPrice)/lines, 5*min_price_step)
+		local clasterStep = math.max((maxPrice - minPrice)/lines, min_price_step)
 
 		--WriteLog('minPrice '..tostring(minPrice)..' maxPrice '..tostring(maxPrice)..' clasterStep '..tostring(clasterStep))
 		
@@ -170,16 +189,21 @@ function FFF()
 					outlines[i].val = sortedProfile[i].price                           
 				end                                               
 			end                   
-			SetValue(index-shift,       i, outlines[i].val)
-			SetValue(outlines[i].index, i, outlines[i].val)
+			SetValue(index-shift,       i+1, outlines[i].val)
+			SetValue(outlines[i].index, i+1, outlines[i].val)
 					
 			--WriteLog('line '..tostring(i).." price "..tostring(GetValue(index-shift, i)).." - "..tostring(GetValue(outlines[i].index, i)).." vol "..tostring(outlines[i].index-index+shift))
 				
 		end                
 
+		if showMaxLine==1 then
+			SetValue(index-shift, 1, maxPrice)
+			maxPriceLine[index] = maxPrice
+		end
+
 		calculated_buffer[index] = true
 				
-		return nil
+		return maxPriceLine[index]
 	end
 end
 
