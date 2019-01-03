81f9c9e7be9ba2d3afe0391f2e118e32c793455b
nnh <nick-h@yandex.ru>
2019-01-03 21:09:00 +0300

    Update testTHV_HA.lua

--- tester/testTHV_HA.lua
+++ tester/testTHV_HA.lua
@@ -1,10 +1,11 @@
-
 -------------------------
 --THV
 
 THVSettings = {
     period    = 1,
-    koef = 1 
+    koef = 1, 
+    shift = 1,
+    Size = 0
 }
 
 function initTHV()
@@ -21,143 +22,48 @@ function initTHV()
 	cache_O=nil
 	cache_C=nil
     trend=nil
+    calcChartResults=nil
+    ATR=nil
+    calcATR = true
 end
 
 function iterateTHV(iSec, cell)
     
-    Clear(tres_id)
+    iterateSLTP = false
 
-    myLog("================================================")
-    myLog("Sec code "..SEC_CODES['sec_codes'][iSec])
-
-    --local settings = ALGORITHMS["settings"][cell]
-    local Size = GetCell(t_id, lineTask, 2).value or SEC_CODES['Size'][iSec]
-    --resultsTable = {}
-    clearResultsTable(iSec, cell)
-    local resultsTable = CreateResTable(iSec)    
-    local count = #resultsTable
-    local settingTable = ALGORITHMS['settings'][cell]
-
-    myLog("Interval "..ALGORITHMS['names'][cell])
-    myLog("================================================")
-
-    param1Min = 4
+    param1Min = 8
     param1Max = 112
     param1Step = 1
 
-    param2Min = 0.5
-    param2Max = 2
+    param2Min = 0.3
+    param2Max = 2.1
     param2Step = 0.1
-    
-    param3Min = 0
-    param3Max = 0
+
+    param3Min = 1
+    param3Max = 38
     param3Step = 1
-
-    local ChartId = SEC_CODES['ChartId'][iSec]
-    if ChartId ~= nil then
-        DelAllLabels(ChartId);
-    end
-   
-    maxProfitIndex = 0
-    maxProfit = nil
-    maxProfitDeals = nil
-    maxProfitAlgoResults = nil
-
-    local localCount = 0
-    local done = 0
-
-    DS = DataSource(iSec)
-    beginIndex = DS:Size()-Size
-    endIndex = DS:Size()
-
-	local rescount = 0
     
-    local allCount = ((param1Max-param1Min + param1Step)/param1Step)*((param2Max - param2Min + param2Step)/param2Step)*((param3Max - param3Min + param3Step)/param3Step)
+    local settingsTable = {}
+    local allCount = 0
 
-    for _period = param1Min, param1Max, param1Step do
-        for _koef = param2Min, param2Max, param2Step do
-            for _SarDeviation = param3Min, param3Max, param3Step do
-                
-                count = count + 1
-                localCount = localCount + 1
-                done = round(localCount*100/allCount, 0)
-                SetCell(t_id, lineTask, 4, tostring(done).."%", done)
-
-                allProfit = 0
-                shortProfit = 0
-                longProfit = 0
-                lastDealPrice = 0
-                dealsCount = 0
-                dealsLongCount = 0
-                dealsShortCount = 0
-                profitDealsLongCount = 0
-                profitDealsShortCount = 0
-                ratioProfitDeals = 0
-                initalAssets = 0
-                        
-                settingsTask = {
-                    period    = _period,                   
-                    koef = _koef,                  
-                    Size = Size
-                }
-            
-                calculateAlgorithm(iSec, cell)
-                local profitRatio, avg, sigma, maxDrawDown, sharpe, AHPR, ZCount = calculateSigma(deals)
-
-                --myLog("--------------------------------------------------")
-                --myLog("Прибыль по лонгам "..tostring(longProfit))
-                --myLog("Прибыль по шортам "..tostring(shortProfit))
-                --myLog("Прибыль всего "..tostring(allProfit))
-                --myLog("================================================")
-                
-                dealsLP = tostring(dealsLongCount).."/"..tostring(profitDealsLongCount)
-                dealsSP = tostring(dealsShortCount).."/"..tostring(profitDealsShortCount)
-                if dealsLongCount + dealsShortCount > 0 then
-                    ratioProfitDeals = round((profitDealsLongCount + profitDealsShortCount)*100/(dealsLongCount + dealsShortCount), 2)
-                end
-                
-                if profitRatio ~= nil then
-                    rescount = rescount + 1
-                    resultsTable[rescount] = {iSec, cell, allProfit, profitRatio, longProfit, shortProfit, dealsLP, dealsSP, ratioProfitDeals, avg, sigma, maxDrawDown, sharpe, AHPR, ZCount, settingsTask}
-
-                    if maxProfit == nil or maxProfit<allProfit then
-                        maxProfit = allProfit
-                        maxProfitIndex = rescount
-                        maxProfitDeals = deals
-                        maxProfitAlgoResults = algoResults
-                        SetCell(t_id, lineTask, 5, tostring(allProfit), allProfit)
-                        SetCell(t_id, lineTask, 6, tostring(profitRatio), profitRatio)
-                        SetCell(t_id, lineTask, 7, tostring(longProfit), longProfit)
-                        SetCell(t_id, lineTask, 8, tostring(shortProfit), shortProfit)
-                        SetCell(t_id, lineTask, 9, tostring(dealsLP), 0)
-                        SetCell(t_id, lineTask, 10, tostring(dealsSP), 0)
-                        SetCell(t_id, lineTask, 11, tostring(ratioProfitDeals), ratioProfitDeals)
-                        SetCell(t_id, lineTask, 12, tostring(avg), avg)
-                        SetCell(t_id, lineTask, 13, tostring(sigma), sigma)
-                        SetCell(t_id, lineTask, 14, tostring(maxDrawDown), maxDrawDown)
-                        SetCell(t_id, lineTask, 15, tostring(sharpe), sharpe)
-                        SetCell(t_id, lineTask, 16, tostring(AHPR), AHPR)
-                        SetCell(t_id, lineTask, 17, tostring(ZCount), ZCount)
-                    end
-                end
-    
+    for param1 = param1Min, param1Max, param1Step do               
+        for param2 = param2Min, param2Max, param2Step do    
+            local calculatedShift = {}
+            for param3 = param3Min, math.ceil(0.8*param1), param3Step do
+            --for param3 = param3Min, param3Max, param3Step do
+                    allCount = allCount + 1
+                    
+                    settingsTable[allCount] = {
+                        period    = param1,                   
+                        koef    =   param2,                   
+                        shift    = param3,
+                        Size = Size
+                    }
             end
         end
     end
-       
-    SetCell(t_id, lineTask, 4, "100%", 100)
 
-    --if count > 1 then
-    --    table.sort(resultsTable, function(a,b) return a[4]<b[4] end)
-    --end
- 
-    openResults(resultsTable, settingTable)
-
-    if ChartId ~= nil then
-        addDeals(maxProfitDeals, ChartId, DS)
-        stv.UseNameSpace(ChartId)
-        stv.SetVar('algoResults', maxProfitAlgoResults)
-    end
+    iterateAlgorithm(iSec, cell, settingsTable)
 
 end
 
@@ -165,6 +71,12 @@ function THV(index, settings, DS)
 
     local period = settings.period or 32
     local koef = settings.koef or 1
+    local shift = settings.shift or 1
+
+    local indexToCalc = 1000
+    indexToCalc = settings.Size or indexToCalc
+    local beginIndexToCalc = settings.beginIndexToCalc or math.max(1, settings.beginIndex - indexToCalc)
+    local endIndexToCalc = settings.endIndex or DS:Size()
 
     if index == nil then index = 1 end
     
@@ -189,13 +101,11 @@ function THV(index, settings, DS)
     local highHA
     local lowHA
     
-    local outDown
-    local outUP
-
     if g_ibuf_96 == nil then
+
         myLog("Показатель Period "..tostring(period))
         myLog("Показатель koef "..tostring(koef))
-        myLog("Показатель SarDeviation "..tostring(SarDeviation))
+        myLog("Показатель shift "..tostring(shift))
         myLog("--------------------------------------------------")
         
         g_ibuf_92={}
@@ -219,16 +129,20 @@ function THV(index, settings, DS)
         gda_120[index]=0
         gda_124[index]=0
         gda_128[index]=0
-        
+
         trend = {}
         trend[index] = 1
+        calcChartResults = {}
+        calcChartResults[index] = {}
         
         cache_O = {}
         cache_C = {}
         cache_O[index]= 0
         cache_C[index]= 0
+        ATR = {}
+        ATR[index] = 0			
 
-        return g_ibuf_96
+        return cache_O, nil, calcChartResults
     end
 
     g_ibuf_92[index] = g_ibuf_92[index-1] 
@@ -243,9 +157,29 @@ function THV(index, settings, DS)
     gda_128[index] = gda_128[index-1] 
     
     trend[index] = trend[index-1] 
+    calcChartResults[index] = calcChartResults[index-1] 
 
     cache_O[index] = cache_O[index-1] 
     cache_C[index] = cache_C[index-1] 
+    ATR[index] = ATR[index-1]
+
+    if index<period then
+        ATR[index] = 0
+    elseif index==period then
+        local sum=0
+        for i = 1, period do
+            sum = sum + dValue(i)
+        end
+        ATR[index]=sum / period
+    elseif index>period then
+        ATR[index]=(ATR[index-1] * (period-1) + dValue(index)) / period
+    end
+
+    if index <= beginIndexToCalc + (period + shift + 1) or index > endIndexToCalc then
+        return cache_O, nil, calcChartResults
+    end
+
+    local typeVal = 'C'
 
     if DS:C(index) ~= nil then        
         
@@ -254,7 +188,9 @@ function THV(index, settings, DS)
 			previous = FindExistCandle(previous)
 		end
 
-        gda_108[index] = gd_172 * DS:C(index) + gd_180 * (gda_108[previous])
+        local val = dValue(index,typeVal)
+
+        gda_108[index] = gd_172 * val + gd_180 * (gda_108[previous])
 		gda_112[index] = gd_172 * (gda_108[index]) + gd_180 * (gda_112[previous])
 		gda_116[index] = gd_172 * (gda_112[index]) + gd_180 * (gda_116[previous])
 		gda_120[index] = gd_172 * (gda_116[index]) + gd_180 * (gda_120[previous])
@@ -268,35 +204,40 @@ function THV(index, settings, DS)
 		g_ibuf_96[index] = ld_0
 		g_ibuf_100[index] = ld_0
         
-		cache_O[index]=DS:O(index)
-		cache_C[index]=DS:C(index)
+		--cache_O[index]=DS:O(index)
+		--cache_C[index]=DS:C(index)
 
-        openHA = (cache_O[previous] + cache_C[previous])/2
-		closeHA = (DS:O(index) + DS:H(index) + DS:L(index) + DS:C(index))/4
-		highHA = math.max(DS:H(index), math.max(openHA, closeHA))
-		lowHA = math.min(DS:L(index), math.min(openHA, closeHA))
-		
-		cache_O[index] = openHA
-		cache_C[index] = closeHA
-		
-		if openHA < closeHA then
-			outDown = nil
-			outUP = (DS:H(index) + DS:L(index))/2
-		elseif openHA > closeHA then
-			outDown = (DS:H(index) + DS:L(index))/2
-			outUP = nil
-		end
-
-        if ld_8 > ld_0 and cache_O[index] > cache_C[index] and cache_O[index-1] > cache_C[index-1] then 
-			trend[index] = -1
-        end
-        if ld_8 < ld_0 and cache_O[index] < cache_C[index] and cache_O[index-1] < cache_C[index-1] then
-			trend[index] = 1
-		end
-
-    end
+        --if ld_8 > ld_0 and cache_O[index] > cache_C[index] and cache_O[index-1] > cache_C[index-1] then 
+		--	trend[index] = -1
+        --end
+        --if ld_8 < ld_0 and cache_O[index] < cache_C[index] and cache_O[index-1] < cache_C[index-1] then
+		--	trend[index] = 1
+		--end
+        --if g_ibuf_96[index] > DS:C(index) and cache_O[index] > cache_C[index] and cache_O[index-1] > cache_C[index-1] then 
+		--	trend[index] = -1
+        --end
+        --if g_ibuf_96[index] < DS:C(index) and cache_O[index] < cache_C[index] and cache_O[index-1] < cache_C[index-1]  then 
+		--	trend[index] = 1 
+        --end
             
-    return g_ibuf_96, trend 
+        local isUpPinBar = DS:C(index)>DS:O(index) and (DS:H(index)-DS:C(index))/(DS:H(index) - DS:L(index))>=0.5 
+        local isLowPinBar = DS:C(index)<DS:O(index) and (DS:C(index)-DS:L(index))/(DS:H(index) - DS:L(index))>=0.5 
+    
+        local isBuy = (not isUpPinBar and g_ibuf_96[index] > g_ibuf_96[index-shift] and g_ibuf_96[index-1] <= g_ibuf_96[index-shift-1]) --and cache_O[index] > cache_C[index-shift] 
+        local isSell = (not isLowPinBar and g_ibuf_96[index] < g_ibuf_96[index-shift] and g_ibuf_96[index-1] >= g_ibuf_96[index-shift-1]) --and cache_O[index] > cache_C[index-shift]
+        
+        if isBuy then
+            trend[index] = 1
+        end
+        if isSell then
+            trend[index] = -1
+        end
+        
+    end
+    
+    calcChartResults[index] = {g_ibuf_96[index], g_ibuf_96[index-shift-1]}
+
+    return g_ibuf_96, trend, calcChartResults
     
 end
 
