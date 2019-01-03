4621f7be582916de5681ea0ec709305043024ddf
nnh <nick-h@yandex.ru>
2019-01-03 21:13:01 +0300

    Create barindex.lua

---
+++ tester/barindex.lua
@@ -0,0 +1,21 @@
+Settings = {}
+Settings.Name = '*bar index'
+Settings.line = 
+{
+    {
+    Name = "bar index",
+    Color = RGB(0, 0, 0),
+    Type = TYPE_HISTOGRAM,
+    Width = 2
+    }
+}
+
+function Init()
+    return #Settings.line
+end
+ 
+function OnCalculate(index)	
+	return index
+end
+
+
