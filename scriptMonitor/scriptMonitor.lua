4ceb14f4f36e37d5226dba023da243b7519c4a09
nnh <nick-h@yandex.ru>
2019-03-01 17:25:48 +0300

    fix round function

--- scriptMonitor/scriptMonitor.lua
+++ scriptMonitor/scriptMonitor.lua
@@ -1510,7 +1510,7 @@ function PaySoundFile(file_name)
 end
 
 function round(num, idp)
-    if idp and num then
+    if num then
     local mult = 10^(idp or 0)
     if num >= 0 then return math.floor(num * mult + 0.5) / mult
     else return math.ceil(num * mult - 0.5) / mult end
