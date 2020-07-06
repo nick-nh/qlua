d50785c42e290dc97cd36677929911dc2fdd8a75
nnh <nick-h@yandex.ru>
2020-07-06 16:51:09 +0300

    lua 5.3 update

--- priceAvgProfile.lua
+++ priceAvgProfile.lua
@@ -1,216 +1,270 @@
 -- nnh Glukk Inc. nick-h@yandex.ru
 
---logfile=io.open(getWorkingFolder().."\\LuaIndicators\\priceAvgProfile.txt", "w")
+local logFile = nil
+-- logFile = io.open(_G.getWorkingFolder().."\\LuaIndicators\\priceAvgProfile.log", "w")
 
-Settings={}
-Settings.period = 150
-Settings.shift = 100
-Settings.Name = "*priceAvgProfile"
-Settings.weeks = 0 -- 1 - текущая, отрицательное число - сколько прошлых недель, включая текущую
-Settings.fixShift = 1 -- 1 - всегда смещено на указанное количество shift, если 0, то будет смещено на дату начала неделеи расчета
-Settings.showMaxLine = 1
+_G.unpack = rawget(table, "unpack") or _G.unpack
+
+_G.Settings = {}
+_G.Settings.period 			= 150
+_G.Settings.shift 			= 100
+_G.Settings.barShift 		= 0
+_G.Settings.Name 			= "*priceAvgProfile"
+_G.Settings.weeks 			= 0 -- 1 - текущая, отрицательное число - сколько прошлых недель, включая текущую
+_G.Settings.fixShift 		= 1 -- 1 - всегда смещено на указанное количество shift, если 0, то будет смещено на дату начала неделеи расчета
+_G.Settings.showMaxLine 	= 1
 ---------------------------------------------------------------------------------------
 
-lines = 100
-scale = 2
-min_price_step = 1
+local lines 			= 100
+local scale 			= 2
+local min_price_step 	= 1
+local error_log      	= {}
 
-function Init()
-	Settings.line = {}
-	Settings.line[1] = {}
-	Settings.line[1] = {Name = 'maxVol', Color = RGB(255, 128, 64), Type = TYPE_LINE, Width = 2}
-	for i = 1, lines do
-		Settings.line[i+1] = {}
-		Settings.line[i+1] = {Color = RGB(185, 185, 185), Type = TYPE_LINE, Width = 2}
-	end
-	
-	myFFF = FFF()
-	return lines
+local math_max      	= math.max
+local math_min      	= math.min
+local math_floor      	= math.floor
+local math_ceil      	= math.ceil
+local math_pow      	= math.pow
+local os_time	    	= os.time
+local PlotLines     	= function() end
+
+
+local O    				= _G['O']
+local C    				= _G['C']
+local H    				= _G['H']
+local L    				= _G['L']
+local V    				= _G['V']
+local T    				= _G['T']
+local Size 				= _G['Size']
+local SetValue 			= _G['SetValue']
+local CandleExist 		= _G['CandleExist']
+local message       	= _G['message']
+
+
+local function log_tostring(...)
+    local n = select('#', ...)
+    if n == 1 then
+	    return tostring(select(1, ...))
+    end
+    local t = {}
+    for i = 1, n do
+	    t[#t + 1] = tostring((select(i, ...)))
+    end
+    return table.concat(t, " ")
 end
 
-function OnCalculate(index)
-	if index == 1 then
-		DSInfo = getDataSourceInfo()     	
-		min_price_step = getParamEx(DSInfo.class_code, DSInfo.sec_code, "SEC_PRICE_STEP").param_value
-		scale = getSecurityInfo(DSInfo.class_code, DSInfo.sec_code).scale
-	end	
-	return myFFF(index, Settings)
+local function myLog(...)
+	if logFile==nil then return end
+    logFile:write(tostring(os.date("%c",os_time())).." "..log_tostring(...).."\n");
+    logFile:flush();
 end
 
 ---------------------------------------------------------------------------------------
-function FFF()
+local function Algo(Fsettings)
 
-	local cacheL={}
-	local cacheH={}
-    local cacheC={}
-    local weeksBegin={}
-    local maxPriceLine={}
+    local period 		= Fsettings.period or 150
+    local shift 		= Fsettings.shift or 150
+    local barShift 		= Fsettings.barShift or 0
+    local weeks 		= Fsettings.weeks or 0
+    local fixShift 		= Fsettings.fixShift or 0
+    local showMaxLine 	= Fsettings.showMaxLine or 0
+    local bars 			= 50
 
-	local outlines = {}
+	-- myLog('Fsettings '..tostring(Fsettings))
+
+	shift = math_max(bars+1, shift)
+
+	local cacheL		= {}
+	local cacheH		= {}
+    local cacheC		= {}
+    local weeksBegin    = {}
+    local maxPriceLine	= {}
+
+    error_log 			= {}
+
+	local outlines 		= {}
 	local calculated_buffer={}
-	
-	return function(ind, Fsettings)
 
-        local period = Fsettings.period or 150
-        local shift = Fsettings.shift or 150
-        local weeks = Fsettings.weeks or 0
-        local fixShift = Fsettings.fixShift or 0
-        local showMaxLine = Fsettings.showMaxLine or 0
-        local bars = 50
+	return function(index)
 
-		shift = math.max(bars+1, shift)
+        local status, res = pcall(function()
 
-		local index = ind
+			if index == 1 then
+	            maxPriceLine = {}
+	            weeksBegin = {}
+	            cacheL = {}
+	            cacheL[index] = 0
+	            cacheH = {}
+	            cacheH[index] = 0
+	            cacheC = {}
+	            cacheC[index] = 0
 
-		if index == 1 then			
-            maxPriceLine = {}
-            weeksBegin = {}
-            cacheL = {}
-            cacheL[index] = 0			
-            cacheH = {}
-            cacheH[index] = 0			
-            cacheC = {}
-            cacheC[index] = 0			
+				calculated_buffer = {}
+	            outlines = {}
 
-			calculated_buffer = {}
-            outlines = {}
+				return nil
+			end
 
-			return nil
-		end
-	------------------------------		
+			--maxPriceLine[index] = maxPriceLine[index-1]
+			cacheL[index] = cacheL[index-1]
+			cacheH[index] = cacheH[index-1]
+			cacheC[index] = cacheC[index-1]
 
-		--maxPriceLine[index] = maxPriceLine[index-1] 
-		cacheL[index] = cacheL[index-1] 
-		cacheH[index] = cacheH[index-1] 
-		cacheC[index] = cacheC[index-1] 
+			if not CandleExist(index) then
+				return maxPriceLine[index]
+			end
 
-		if not CandleExist(index) then
-			return maxPriceLine[index]
-		end
+			cacheH[index] = H(index)
+	        cacheL[index] = L(index)
+	        cacheC[index] = C(index)
 
-		cacheH[index] = H(index)
-        cacheL[index] = L(index)
-        cacheC[index] = C(index)
-		
-		if T(index).week_day<T(index-1).week_day or T(index).year>T(index-1).year then
-			weeksBegin[#weeksBegin+1] = index
-		end
+			-- myLog('index '..tostring(index)..' T ', tostring(T(index)))
+			if T(index).week_day<T(index-1).week_day or T(index).year>T(index-1).year then
+				weeksBegin[#weeksBegin+1] = index
+			end
 
-		if index < Size() then return nil end	
+			if index ~= Size()-barShift then return nil end
 
-		if calculated_buffer[index] ~= nil then
-			return maxPriceLine[index]
-		end
+			if calculated_buffer[index] ~= nil then
+				return maxPriceLine[index]
+			end
 
-		if showMaxLine==1 then
-			SetValue(index-shift-1, 1, nil)
-			SetValue(index-shift,   1, nil)
-		end
+			if showMaxLine==1 then
+				SetValue(index-shift-1, 1, nil)
+				SetValue(index-shift,   1, nil)
+				SetValue(index-1,   	1, nil)
+			end
 
-		for i=1,#outlines do                   		
-			SetValue(index-shift-1,          i+1, nil)
-			SetValue(index-shift,            i+1, nil)
-			SetValue(outlines[i].index,   	 i+1, nil)
-			
-			outlines[i].index = index-shift
-			outlines[i].val = nil
-		end            
-		
-		local beginIndex = index-period
-		if weeks == 1 then
-			beginIndex = weeksBegin[#weeksBegin] or beginIndex
-		end
-		if weeks < 0 then
-			beginIndex = weeksBegin[#weeksBegin+weeks] or beginIndex
-		end
-		
-		if fixShift==0 then
-			shift = math.max(bars+1, index-beginIndex)
-		end
+			for i=1,#outlines do
+				SetValue(index-shift-1,          i+1, nil)
+				SetValue(index-shift,            i+1, nil)
+				SetValue(outlines[i].index,   	 i+1, nil)
 
-		--WriteLog('weeks '..tostring(weeks)..' last '..tostring(weeksBegin[#weeksBegin])..' beginIndex '..tostring(beginIndex))
-		
-		local maxPrice = math.max(unpack(cacheH,math.max(beginIndex, 1),index))
-		local minPrice = math.min(unpack(cacheL,math.max(beginIndex, 1),index))       
-		
-		----------------------------------------
-		local priceProfile = {}
-		local clasterStep = math.max((maxPrice - minPrice)/lines, min_price_step)
+				outlines[i].index = index-shift
+				outlines[i].val = nil
+			end
 
-		--WriteLog('minPrice '..tostring(minPrice)..' maxPrice '..tostring(maxPrice)..' clasterStep '..tostring(clasterStep))
-		
-		for i = 0, (index-beginIndex) do
-			if CandleExist(index-i) then				
-				local barSteps = math.max(math.ceil((H(index-i) - L(index-i))/clasterStep),1)
-				for j=0,barSteps-1 do
-					local clasterPrice = math.floor((L(index-i) + j*clasterStep)/clasterStep)*clasterStep
-					local clasterIndex = clasterPrice*math.pow(10, scale)
-					if priceProfile[clasterIndex] == nil then
-						priceProfile[clasterIndex] = {price = clasterPrice, vol = 0}
+			local beginIndex = index-period
+			if weeks == 1 then
+				beginIndex = weeksBegin[#weeksBegin] or beginIndex
+			end
+			if weeks < 0 then
+				beginIndex = weeksBegin[#weeksBegin+weeks] or beginIndex
+			end
+
+			if fixShift==0 then
+				shift = math_max(bars+1, index-beginIndex)
+			end
+
+			-- myLog('weeks '..tostring(weeks)..' last '..tostring(weeksBegin[#weeksBegin])..' beginIndex '..tostring(beginIndex))
+
+			local maxPrice = math_max(unpack(cacheH,math_max(beginIndex, 1),index))
+			local minPrice = math_min(unpack(cacheL,math_max(beginIndex, 1),index))
+
+			----------------------------------------
+			local priceProfile = {}
+			local clasterStep = math_max((maxPrice - minPrice)/lines, min_price_step)
+
+			-- myLog('minPrice '..tostring(minPrice)..' maxPrice '..tostring(maxPrice)..' clasterStep '..tostring(clasterStep))
+
+			for i = 0, (index-beginIndex) do
+				if CandleExist(index-i) then
+					local barSteps = math_max(math_ceil((H(index-i) - L(index-i))/clasterStep),1)
+					for j=0,barSteps-1 do
+						local clasterPrice = math_floor((L(index-i) + j*clasterStep)/clasterStep)*clasterStep
+						local clasterIndex = clasterPrice*math_pow(10, scale)
+						if priceProfile[clasterIndex] == nil then
+							priceProfile[clasterIndex] = {price = clasterPrice, vol = 0}
+						end
+						priceProfile[clasterIndex].vol = priceProfile[clasterIndex].vol + V(index-i)/barSteps
 					end
-					priceProfile[clasterIndex].vol = priceProfile[clasterIndex].vol + V(index-i)/barSteps
 				end
 			end
-		end
 
-		--------------------
-		local MAXV = 0
-		local maxPrice = 0
-		local maxCount = 0 
+			--------------------
+			local MAXV 			= 0
+			local maxVolPrice 	= 0
+			local maxCount 		= 0
 
-		local sortedProfile = {}
+			local sortedProfile = {}
 
-		for i, profileItem in pairs(priceProfile) do
-			MAXV=math.max(MAXV,profileItem.vol)
-			if MAXV == profileItem.vol then
-				maxPrice=profileItem.price
+			for _, profileItem in pairs(priceProfile) do
+				MAXV=math_max(MAXV,profileItem.vol)
+				if MAXV == profileItem.vol then
+					maxVolPrice=profileItem.price
+				end
+				maxCount = maxCount + 1
+				sortedProfile[maxCount] = {price = profileItem.price, vol = profileItem.vol}
 			end
-			maxCount = maxCount + 1
-			sortedProfile[maxCount] = {price = profileItem.price, vol = profileItem.vol}
-		end
-				
-		--WriteLog('maxV '..tostring(MAXV)..' tblMax '..tostring(sortedProfile[1].vol))
 
-		if maxPrice == 0 then
-			maxPrice = O(index) 
-		end
+			-- myLog('maxV '..tostring(MAXV)..' tblMax '..tostring(sortedProfile[1].vol))
 
-		table.sort(sortedProfile, function(a,b) return (a['vol'] or 0) > (b['vol'] or 0) end)
+			if maxVolPrice == 0 then
+				maxVolPrice = O(index)
+			end
 
-		---------------------
-		for i=1,lines do                                        
+			table.sort(sortedProfile, function(a,b) return (a['vol'] or 0) > (b['vol'] or 0) end)
 
-			outlines[i] = {index = index-shift+bars, val = maxPrice}
+			---------------------
+			for i=1,lines do
 
-			if sortedProfile[i]~=nil then				
-				sortedProfile[i].vol=math.floor(sortedProfile[i].vol/MAXV*bars)
-				if sortedProfile[i].vol>0 then
-					outlines[i].index = index-shift+sortedProfile[i].vol
-					outlines[i].val = sortedProfile[i].price                           
-				end                                               
-			end                   
-			SetValue(index-shift,       i+1, outlines[i].val)
-			SetValue(outlines[i].index, i+1, outlines[i].val)
-					
-			--WriteLog('line '..tostring(i).." price "..tostring(GetValue(index-shift, i)).." - "..tostring(GetValue(outlines[i].index, i)).." vol "..tostring(outlines[i].index-index+shift))
-				
-		end                
+				outlines[i] = {index = index-shift+bars, val = maxVolPrice}
 
-		if showMaxLine==1 then
-			SetValue(index-shift, 1, maxPrice)
-			maxPriceLine[index] = maxPrice
-		end
+				if sortedProfile[i]~=nil then
+					sortedProfile[i].vol=math_floor(sortedProfile[i].vol/MAXV*bars)
+					if sortedProfile[i].vol>0 then
+						outlines[i].index = index-shift+sortedProfile[i].vol
+						outlines[i].val = sortedProfile[i].price
+					end
+				end
+				SetValue(index-shift,       i+1, outlines[i].val)
+				SetValue(outlines[i].index, i+1, outlines[i].val)
 
-		calculated_buffer[index] = true
-				
+				--myLog('line '..tostring(i).." price "..tostring(GetValue(index-shift, i)).." - "..tostring(GetValue(outlines[i].index, i)).." vol "..tostring(outlines[i].index-index+shift))
+
+			end
+
+			if showMaxLine==1 then
+				SetValue(index-shift, 1, maxVolPrice)
+				maxPriceLine[index] = maxVolPrice
+			end
+
+			calculated_buffer[index] = true
+
+
+        end)
+        if not status then
+            if not error_log[tostring(res)] then
+                error_log[tostring(res)] = true
+                myLog(tostring(res))
+                message(tostring(res))
+            end
+        end
+
 		return maxPriceLine[index]
 	end
 end
 
-function WriteLog(text)
 
-    logfile:write(tostring(os.date("%c",os.time())).." "..text.."\n");
-    logfile:flush();
-    LASTLOGSTRING = text;
- 
+function _G.Init()
+	_G.Settings.line = {}
+	_G.Settings.line[1] = {}
+	_G.Settings.line[1] = {Name = 'maxVol', Color = _G.RGB(255, 128, 64), Type = _G.TYPE_LINE, Width = 2}
+	for i = 1, lines do
+		_G.Settings.line[i+1] = {}
+		_G.Settings.line[i+1] = {Color = _G.RGB(185, 185, 185), Type = _G.TYPE_LINE, Width = 2}
+	end
+
+	PlotLines = Algo(_G.Settings)
+	return lines
 end
+
+function _G.OnCalculate(index)
+	if index == 1 then
+		local DSInfo 	= _G.getDataSourceInfo()
+		min_price_step 	= tonumber(_G.getParamEx(DSInfo.class_code, DSInfo.sec_code, "SEC_PRICE_STEP").param_value) or 0
+		scale 			= tonumber(_G.getSecurityInfo(DSInfo.class_code, DSInfo.sec_code).scale) or 0
+	end
+	return PlotLines(index)
+end
+
