4c0cc0348b5a3fc68e7939de7627822e7703c325
nnh <nick-h@yandex.ru>
2020-10-11 23:17:02 +0300

    Add GetIncomeMessages

--- telegramQuik/luaPipe/dllmain.cpp
+++ telegramQuik/luaPipe/dllmain.cpp
@@ -47,16 +47,9 @@ static void lua_print(lua_State* LST, const char* text)
 
 }
 
-static int forLua_SendTMessage(lua_State* LST)
+static HANDLE get_pipe(lua_State* LST, char* pipe_name)
 {
-    const char* msg     = lua_tostring(LST, 1);
-    const char* pipe_ch = lua_tostring(LST, 2);
-    char pipe_name[100];
-    sprintf_s(pipe_name, "\\\\.\\pipe\\%s", pipe_ch);
-    LPCTSTR lpvMessage    = msg;
     HANDLE hPipe;
-    BOOL   fSuccess = FALSE;
-    DWORD  cbToWrite, cbWritten, dwMode;
     LPCTSTR lpszPipename = pipe_name;
     char outString[200];
 
@@ -67,6 +60,7 @@ static int forLua_SendTMessage(lua_State* LST)
         hPipe = CreateFile(
             lpszPipename,   // pipe name 
             // read and write access 
+            GENERIC_READ |  // read and write access 
             GENERIC_WRITE,
             0,              // no sharing 
             NULL,           // default security attributes
@@ -85,7 +79,7 @@ static int forLua_SendTMessage(lua_State* LST)
         {
             sprintf_s(outString, "Could not open pipe %s. GLE=%d", pipe_name, GetLastError());
             lua_print(LST, outString);
-            return 1;
+            return NULL;
         }
 
         // All pipe instances are busy, so wait for 20 seconds. 
@@ -93,15 +87,21 @@ static int forLua_SendTMessage(lua_State* LST)
         if (!WaitNamedPipe(lpszPipename, 20000))
         {
             lua_print(LST, "Could not open pipe: 20 second wait timed out");
-            return 1;
+            return NULL;
         }
     }
 
-    // Send a message to the pipe server. 
+    return hPipe;
 
-    cbToWrite = (lstrlen(lpvMessage) + 1) * sizeof(TCHAR);
-    //sprintf_s(outString, "Write msg to pipe length %d, byte %d", lstrlen(lpvMessage) + 1, cbToWrite);
-    //lua_print(LST, outString);
+}
+
+static BOOLEAN SendMessageToPipe(lua_State* LST, HANDLE hPipe, LPCTSTR lpvMessage, DWORD  cbToWrite)
+{
+    BOOL   fSuccess = FALSE;
+    DWORD  cbWritten;
+    char outString[200];
+
+    // Send a message to the pipe server. 
 
     fSuccess = WriteFile(
         hPipe,                  // pipe handle 
@@ -114,18 +114,126 @@ static int forLua_SendTMessage(lua_State* LST)
     {
         sprintf_s(outString, "Write msg to pipe failed. GLE=%d", GetLastError());
         lua_print(LST, outString);
+        return NULL;
+    }
+
+    return TRUE;
+}
+
+static int forLua_SendTMessage(lua_State* LST)
+{
+    const char* msg     = lua_tostring(LST, 1);
+    const char* pipe_ch = lua_tostring(LST, 2);
+    char pipe_name[100];
+    sprintf_s(pipe_name, "\\\\.\\pipe\\%s", pipe_ch);
+
+    LPCTSTR lpvMessage    = msg;
+
+    char outString[200];
+
+    // Try to open a named pipe; wait for it, if necessary. 
+    HANDLE hPipe = get_pipe(LST, pipe_name);
+
+    if (hPipe == NULL)
+    {
+        lua_pushboolean(LST, FALSE);
+        return NULL;
+    }
+
+    if (!SendMessageToPipe(LST, hPipe, lpvMessage, (lstrlen(lpvMessage) + 1) * sizeof(TCHAR)))
+    {
+        lua_pushboolean(LST, false);
         return 1;
     }
 
-    lua_print(LST, "<Message sent to server>");
+    lua_print(LST, "<Message was sent to server>");
 
     CloseHandle(hPipe);
     lua_pushboolean(LST, true);
     return 1;
 }
 
+static int forLua_GetIncomeMessages(lua_State* LST)
+{
+    const char* pipe_ch = lua_tostring(LST, 1);
+    char pipe_name[100];
+    sprintf_s(pipe_name, "\\\\.\\pipe\\%s", pipe_ch);
+
+    LPCTSTR lpvMessage    = TEXT("GetIncomeMessages()");
+    BOOL   fSuccess = FALSE;
+    TCHAR  chBuf[BUFSIZE]; 
+    DWORD  cbRead, cbToWrite, cbWritten, dwMode;
+    TCHAR*  getMsg;
+    getMsg = new TCHAR[BUFSIZE];
+
+    char outString[200];
+
+    // Try to open a named pipe; wait for it, if necessary. 
+    HANDLE hPipe = get_pipe(LST, pipe_name);
+
+    if (hPipe == NULL)
+    {
+        lua_pushboolean(LST, FALSE);
+        return NULL;
+    }
+
+    //char buffer[sizeof(lpvMessage)];
+    //memcpy(buffer, &lpvMessage, sizeof(lpvMessage));
+
+    if (!SendMessageToPipe(LST, hPipe, lpvMessage, (lstrlen(lpvMessage) + 1) * sizeof(TCHAR)))
+    {
+        lua_pushboolean(LST, false);
+        return 1;
+    }
+
+    //int iteration = 0;
+    //TCHAR* tmp;
+    //tmp = new TCHAR[BUFSIZE];
+
+    do
+    {
+        // Read from the pipe. 
+        //if (iteration > 0)
+        //{
+        //    TCHAR  tmp[sizeof(getMsg)];
+        //    memcpy(tmp, &getMsg, sizeof(getMsg));
+
+        //    getMsg = new TCHAR[sizeof(tmp) + BUFSIZE];
+        //    memcpy(getMsg, &tmp, sizeof(tmp));
+        //    delete[] tmp;
+
+        //};
+
+        fSuccess = ReadFile(
+            hPipe,    // pipe handle 
+            chBuf,    // buffer to receive reply 
+            BUFSIZE * sizeof(TCHAR),  // size of buffer 
+            &cbRead,  // number of bytes read 
+            NULL);    // not overlapped 
+            
+        //memcpy(getMsg, &chBuf, sizeof(chBuf));
+        //chBuf[cbRead] = '\0';
+
+        if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
+            break;
+
+    } while (!fSuccess);  // repeat loop if ERROR_MORE_DATA 
+
+    if (!fSuccess)
+    {
+        sprintf_s(outString, "Read msg from pipe failed. GLE=%d", GetLastError());
+        lua_print(LST, outString);
+        return NULL;
+    }
+
+    CloseHandle(hPipe);
+    lua_pushstring(LST, chBuf);
+    return 1;
+}
+
 static struct luaL_Reg ls_lib[] = {
     {"SendMessage", forLua_SendTMessage},
+    {"GetIncomeMessages", forLua_GetIncomeMessages},
     {NULL, NULL}
 };
 
