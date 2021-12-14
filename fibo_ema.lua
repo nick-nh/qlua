f1b61d7e26614f3aace8201cd8676d677cc06982
nnh <nick-h@yandex.ru>
2021-12-14 17:14:57 +0300

    optimize

--- fibo_ema.lua
+++ fibo_ema.lua
@@ -1,9 +1,29 @@
 
-Settings = 
+_G.unpack = rawget(table, "unpack") or _G.unpack
+
+local logFile = nil
+-- logFile = io.open(_G.getWorkingFolder().."\\LuaIndicators\\fibo_ema.txt", "w")
+
+local math_max          = math.max
+local math_min          = math.min
+local math_abs              = math.abs
+local message           = _G['message']
+local RGB               = _G['RGB']
+local TYPE_LINE         = _G['TYPE_LINE']
+local TYPE_POINT        = _G['TYPE_POINT']
+local C                 = _G['C']
+local O                 = _G['O']
+local H                 = _G['H']
+local L                 = _G['L']
+local CandleExist       = _G['CandleExist']
+local os_time	    	= os.time
+
+
+_G.Settings =
 {
 	Name = "*FiboEma",
 	period = 30,
-	bars = 100,
+	bars   = 100,
 	line=
 	{
 		{
@@ -71,191 +91,130 @@ Settings =
 	}
 }
 
-function dValue(i,param)
-local v = param or "C"
-	if  v == "O" then
-		return O(i)
-	elseif   v == "H" then
-		return H(i)
-	elseif   v == "L" then
-		return L(i)
-	elseif   v == "C" then
-		return C(i)
-	elseif   v == "V" then
-		return V(i)
-	elseif   v == "M" then
-		return (H(i) + L(i))/2
-	elseif   v == "T" then
-		return (H(i) + L(i)+C(i))/3
-	elseif   v == "W" then
-		return (H(i) + L(i)+2*C(i))/4
-	elseif   v == "ATR" then
-		return math.max(math.abs(H(i) - L(i)), math.abs(H(i) - C(i-1)), math.abs(C(i-1) - L(i)))
-	else
-		return C(i)
-	end 
+local PlotLines     = function(index) return index end
+local error_log     = {}
+
+local function log_tostring(...)
+    local n = select('#', ...)
+    if n == 1 then
+    return tostring(select(1, ...))
+    end
+    local t = {}
+    for i = 1, n do
+    t[#t + 1] = tostring((select(i, ...)))
+    end
+    return table.concat(t, " ")
+end
+
+local function myLog(...)
+	if logFile==nil then return end
+    logFile:write(tostring(os.date("%c",os_time())).." "..log_tostring(...).."\n");
+    logFile:flush();
 end
 
 ----------------------------------------------------------
-function FiboEMA()
-	
-	local cache_EMA={}
-	local cache_ATR={}
-	
-	return function(ind, _p, _b)
-		local period = _p
-		local index = ind
-		local bars = _b
-		
-		local inc = 0
-		
-		local HiPrice = 0
-		local LowPrice = 0
-		
-		local top = 0
-		local bottom = 0
-		local k = 2/(_p+1)
-		local kk = 2/(_b+1)
+function Algo(Fsettings)
 
-		if index == 1 then
-			cache_EMA = {}
-			cache_ATR={}
-			cache_EMA[index]=(C(index)+O(index))/2
-			cache_ATR[index]=0
-			return nil, nil, nil, nil, nil, nil, nil, nil, nil
-		end
+    Fsettings       = (Fsettings or {})
+    local period    = Fsettings.period or 30
+    local bars    	= Fsettings.bars or 100
 
-		cache_EMA[index] = cache_EMA[index-1]
-		cache_ATR[index] = cache_ATR[index-1]
-		if not CandleExist(index) then
-			return
-		end
+    local save_bars = math_max(period, bars)
 
-		cache_EMA[index]=k*C(index)+(1-k)*cache_EMA[index-1]
-		cache_ATR[index] = kk*dValue(index, "ATR")+(1-kk)*cache_ATR[index-1]
-		
+	local k = 2/(period+1)
+	local kk = 2/(bars+1)
 
-		if index <= bars then
-			return nil, nil, nil, nil, nil, nil, nil, nil, nil
-		end
-		
-		for i = index - bars + 1, index, 1 do
-		
-			top = H(i) - cache_EMA[i]
-			bottom = L(i) - cache_EMA[i]
-			
-			if top > HiPrice then
-				HiPrice = top;
+	error_log = {}
+
+    local p_index
+    local l_index
+
+	local cache_EMA = {}
+	local cache_ATR = {}
+	local cache_H 	= {}
+	local cache_L 	= {}
+
+	local inc
+
+	return function(index)
+
+		local status, res = pcall(function()
+
+			if index == 1 then
+				cache_H 			= {}
+				cache_L				= {}
+				cache_H[index] 		= H(index) or 0
+				cache_L[index] 		= L(index) or 0
+				cache_EMA 			= {}
+				cache_ATR			= {}
+				cache_EMA[index] 	= (C(index)+O(index))/2
+				cache_ATR[index] 	= 0
+				p_index 			= index
+				l_index 			= index
+				inc					= 0
+				return
 			end
-	  
-			if bottom < LowPrice then
-				LowPrice = bottom;
+
+			cache_EMA[index] 	= cache_EMA[index-1]
+			cache_ATR[index] 	= cache_ATR[index-1]
+			cache_H[index] 		= cache_H[index-1]
+			cache_L[index] 		= cache_L[index-1]
+
+			if not CandleExist(index) then
+				return
 			end
-			
+
+			if index ~= l_index then p_index = l_index end
+
+			cache_EMA[index]=k*C(index)+(1-k)*cache_EMA[index-1]
+			local ATR = math_max(math_abs(H(index) - L(index)), math_abs(H(index) - C(p_index)), math_abs(C(p_index) - L(index)))
+			cache_ATR[index] = kk*ATR+(1-kk)*cache_ATR[index-1]
+
+			l_index = index
+
+			if index <= bars then
+				return
+			end
+
+			cache_H[index] = H(index) - cache_EMA[index]
+			cache_L[index] = L(index) - cache_EMA[index]
+
+			local high = math_max(unpack(cache_H, index-bars+1, index))
+			local low  = math_min(unpack(cache_L, index-bars+1, index))
+
+			inc = (math_abs(high) > math_abs(low)) and high or low
+			inc = math_abs(inc) + cache_ATR[index]*2
+
+			cache_EMA[index - save_bars] = nil
+			cache_ATR[index - save_bars] = nil
+			cache_H[index - save_bars] 	 = nil
+			cache_L[index - save_bars] 	 = nil
+
+		end)
+		if not status then
+			if not error_log[tostring(res)] then
+				error_log[tostring(res)] = true
+				myLog(tostring(res))
+				message(tostring(res))
+			end
+			return nil
 		end
-		
-		if math.abs(HiPrice) > math.abs(LowPrice) then
-			inc = HiPrice
-		else
-			inc = LowPrice
-		end
-			
-		inc = math.abs(inc) + cache_ATR[index]*2
-		
+
 		return cache_EMA[index], cache_EMA[index]+inc*0.618, cache_EMA[index]+inc*0.5, cache_EMA[index]+inc*0.382, cache_EMA[index]+inc*0.236, cache_EMA[index]-inc*0.236, cache_EMA[index]-inc*0.382, cache_EMA[index]-inc*0.5, cache_EMA[index]-inc*0.618
-		--return cache_EMA[index], cache_EMA[index]+inc*1.618, cache_EMA[index]+inc*1, cache_EMA[index]+inc*0.618, cache_EMA[index]+inc*0.5, cache_EMA[index]-inc*0.5, cache_EMA[index]-inc*0.618, cache_EMA[index]-inc*1, cache_EMA[index]-inc*1.618
 
-			
 	end
 end
 ----------------------------
 
-function Init()
-	myFiboEMA = FiboEMA()
-	return 9
+function _G.Init()
+    PlotLines = Algo(_G.Settings)
+    return 9
 end
 
-function OnCalculate(index)
-	--if index < Settings.period then
-	--	return nil, nil, nil, nil, nil, nil, nil, nil, nil
-	--end
-
-	return myFiboEMA(index, Settings.period, Settings.bars)
+function _G.OnChangeSettings()
+    _G.Init()
 end
 
-function highestHigh(index, period)
-
-	if index == 1 then
-		return nil
-	else
-
-		local highestHigh = H(index)
-		
-		
-		for i = math.max(index - period, 2), index, 1 do
-			
-			if H(i) > highestHigh then
-				highestHigh = H(i)
-			end
-			
-		end
-	
-		return highestHigh 
-	
-	end
-end
-
-function lowestLow(index, period)
-
-	if index == 1 then
-		return nil
-	else
-
-		local lowestLow = L(index)
-		
-		for i = math.max(index - period, 2), index, 1 do
-						
-			if L(i) < lowestLow then
-				lowestLow = L(i)
-			end
-			
-		end
-	
-		return lowestLow 
-	
-	end
-end
-------------------------------------------------------------------
---Вспомогательные функции
-------------------------------------------------------------------
-function round(num, idp)
-if idp and num then
-   local mult = 10^(idp or 0)
-   if num >= 0 then return math.floor(num * mult + 0.5) / mult
-   else return math.ceil(num * mult - 0.5) / mult end
-else return num end
-end
-
-function fWMA()
-
-	return function (Index, Period, ds, idp)
-	local Out = nil
-	   if Index >= Period then
-	  
-		local sum = 0
-		local step = Period
-		
-		  for i = Index-Period+1, Index do
-			if ds[i] == nil then
-				ds[i] = 0
-			end
-			sum = (step*ds[i]) + sum
-			 step = step - 1
-		  end
-		
-		  Out = (2*sum)/(Period*(Period-1))
-	   end
-	   return round(Out,idp)
-	end
-	
+function _G.OnCalculate(index)
+    return PlotLines(index)
 end
