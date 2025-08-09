2362dcc0b010c505b4f0ffea602f982285d3234a
nnh <nick-h@yandex.ru>
2025-08-09 15:33:01 +0300

    relase links, new test

--- telegramQuik/test.lua
+++ telegramQuik/test.lua
@@ -1,14 +1,114 @@
 local path     = _G.getScriptPath and _G.getScriptPath() or (arg[0]:gsub('[^\\/]*$', '', 1):gsub('\\', '/'))
 package.cpath  = path.."/?.dll"
 package.path   = path.."/?.lua"
-local progVersion = '1.02' 	--от 22.08.2024
-local script = {}
-local pipe_name = 'telegram_pipe'
-local strin = [[test from Quik
-emoji ##1F601
-]]
 
---[[
+-- local strin = [[test проверка
+-- вторая строка
+
+-- четвертая @:)
+-- пятая строка
+-- шестая строка
+
+-- emoji ##1F601
+-- ]]
+
+local Sec = {NAME = 'Инструмент', SEC_CODE = 'Код'}
+local pos = 10
+local pos_avg = 123.1
+local stop_loss = 110.2
+local take_profit = 140.3
+local strin = '&#9989 Внимание!\n\n'
+strin = strin..Sec.NAME..', '..Sec.SEC_CODE..'\n\n'
+strin = strin..'Открытие позиции '..(pos > 0 and 'ЛОНГ' or (pos < 0 and 'ШОРТ' or '---'))..'\n\n'
+strin = strin..'&#128309 Цена открытия '..tostring(pos_avg)..'\n\n'
+strin = strin..'&#128308 Уровень стоп-заявки '..tostring(stop_loss)..'\n\n'
+strin = strin..'&#127919 Уровень тейк-профит '..tostring(take_profit)..'\n\n'
+
+
+--Вариант отправки текста
+text = strin
+
+--JSON вариант
+-- text = ([[{
+--     "Message": "strin",
+--     "Telegram": {
+--         "ChatId": "13627367",
+--         "ParseMode": "HTML",
+--         "ReplyToMessageId": "56"
+--     }
+-- }]]):gsub('strin', strin)
+
+
+_G._tostring = tostring
+
+local table_to_string
+table_to_string = function(value, show_number_keys, miss_key, done)
+    local str = ''
+    if show_number_keys == nil then show_number_keys = true end
+    miss_key = miss_key or ''
+
+    done = done or {}
+
+    if (type(value) ~= 'table') then
+        if (type(value) == 'string') then
+            str = string.format("%q", value)
+        else
+            str = _G._tostring(value)
+        end
+      elseif not done [value] then
+        done[value] = true
+        local auxTable = {}
+        local max_index = #value
+        for key in pairs(value) do
+            if type(key) ~= "table" and type(key) ~= "function" and type(key) ~= "boolean" and tostring(key):sub(1,2) ~= '__' then
+                if not miss_key:find(key) and value[key] ~= nil then
+                    if (tonumber(key) ~= key) then
+                        table.insert(auxTable, key)
+                    else
+                        table.insert(auxTable, string.rep('0', max_index-_G._tostring(key):len()).._G._tostring(key))
+                    end
+                end
+            end
+        end
+        table.sort(auxTable)
+
+        str = str..'{'
+        local separator = ""
+        local entry
+        for _, fieldName in ipairs(auxTable) do
+            local prefix = fieldName..' = '
+            if ((tonumber(fieldName)) and (tonumber(fieldName) > 0)) then
+                fieldName = tonumber(fieldName)
+                prefix    = (show_number_keys and "[".._G._tostring(tonumber(fieldName)).."] = " or '')
+            end
+            entry = value[fieldName]
+            -- Check the value type
+            if type(entry) == "table" and getmetatable(entry) == nil then
+                entry = table_to_string(entry, show_number_keys, miss_key, done)
+            elseif type(entry) == "boolean" then
+                entry = _G._tostring(entry)
+            elseif type(entry) == "number" then
+                entry = _G._tostring(entry)
+            else
+                entry = "\"".._G._tostring(entry).."\""
+            end
+            entry = prefix..entry
+            str = str..separator..entry
+            separator = ", "
+        end
+        str = str..'}'
+    end
+    return str
+end
+
+_G.tostring = function(x, ...)
+    if type(x) == "table" and getmetatable(x) == nil then
+        return table_to_string(x, ...)
+    else
+        return _G._tostring(x)
+    end
+end
+
 if _G.message then
    _G.print = function(...)
       local n = select('#', ...)
@@ -23,8 +123,6 @@ if _G.message then
       _G.message(table.concat(t, " "))
    end
 end
-]]
-
 
 --Пример запуска сервера отправки
 --    os.execute('start cmd /c call "'..path..'\\telegramServer\\startTeleServer.bat'..'"')
@@ -39,8 +137,8 @@ local function SendTeleMessage(msg, pipe_name)
    tele_pipe:close() -- закрываем канал
 end
 
+SendTeleMessage(strin, 'telegram_pipe')
 
--- Новая версия чтения. Раздельные каналы. Какнал чтения имеет префик out_
 -- Получение сообщений из канала сервера отправки сообщений
 ---@param pipe_name string
 local function GetTeleMessage(pipe_name)
@@ -80,60 +178,4 @@ if type(str) == 'string' then
       print(t[1])
       print(t[2])
    end
-end
-
-
---- report, из Телеграм получена команда T => Q
-function report(m)
-	message('report, T => Q: '..m)
-end
---- Получение сообщений из канала сервера отправки сообщений
-function getFromChannels()
-	getTQ(pipe_name)
-end
---- Получение сообщений из канала сервера отправки сообщений
-function getTQ(pipe)
-	local str = GetTeleMessage(pipe)
-	if type(str) == 'string' then
-	   local t = assert(load('return '..str))()
-	   if type(t) == 'table' then
-			for i, v in pairs(t) do
-			  message(v)
-			end
-	   end
-	elseif type(str) == 'table' then
-		for i, v in pairs(str) do
-			if v ~= 'No new messages' then
-				---message('T => Q: '..v)
-				report(v)
-			---else
-			---	message('Сообщений нет!')
-			end
-		end
-	end
-
-end
-
-function OnInit(script_path)
-	local Terminal_Version = getInfoParam('VERSION')
-	script.path, script.filename, script.ext = script_path:match("^%s*(.-)([^\\/]-)%.?([^%.\\/]*)%s*$")
-	local mes = 'Start telegramQuik '..script.filename..' '..progVersion..', QUIK '..Terminal_Version
-	message(mes)
-	SendTeleMessage(mes, 'telegram_pipe')
-	is_run = true
-end
-
---- 2.2.24 Функция вызывается терминалом QUIK при остановке скрипта из диалога управления.
-function OnStop()
-	is_run = false
-	local m = 'Stop '..script.filename..'!'
-	message(m) 
-	SendTeleMessage(m, 'telegram_pipe')
-end
-
-function main()
-	while is_run do
-		getFromChannels()
-		sleep(1000)
-	end
 end
